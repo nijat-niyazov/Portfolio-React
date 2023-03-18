8908054c7f6377c66d4fe6887f0f95df8f391c9d
nidzhat-2000 <nidzhat.niyazov@gmail.com>
2023-03-18 15:31:19 +0400

    firebase clean

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,8 +2,6 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
-// const dotenv = require('dotenv');
-// dotenv.config();
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
